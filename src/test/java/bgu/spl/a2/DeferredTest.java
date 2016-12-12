79eb7bad46c6dd407716da63766f1b1f070b7fd6
Alon Blumenreich <alon@mail.com>
2016-12-13 01:38:50 +0200

    Added skeleton file for test VersionMonitor

--- src/test/java/bgu/spl/a2/DeferredTest.java
+++ src/test/java/bgu/spl/a2/DeferredTest.java
@@ -1,5 +1,6 @@
 package bgu.spl.a2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,24 +9,34 @@ import static org.junit.Assert.*;
  * Created by blumenra on 12/12/16.
  */
 public class DeferredTest {
-    @Test
-    public void get() throws Exception {
-
-    }
-
+//    @Test
+//    public void get() throws Exception {
+//
+//
+//    }
+//
     @Test
     public void isResolved() throws Exception {
 
+        Deferred<int[]> deferred = new Deferred<int[]>();
+        int[] ints = new int[10];
+
+        assertFalse(deferred.isResolved());
+
+        deferred.resolve(ints);
+
+        assertTrue(deferred.isResolved());
+
     }
-
-    @Test
-    public void resolve() throws Exception {
-
-    }
-
-    @Test
-    public void whenResolved() throws Exception {
-
-    }
+//
+//    @Test
+//    public void resolve() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void whenResolved() throws Exception {
+//
+//    }
 
 }
