c794ce89c95164228e764864044026dbff85f30f
Alon Blumenreich <alon@mail.com>
2016-12-12 22:18:29 +0200

    Added skeleton file for test VersionMonitor

---
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -0,0 +1,26 @@
+package bgu.spl.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by blumenra on 12/12/16.
+ */
+public class VersionMonitorTest {
+    @Test
+    public void getVersion() throws Exception {
+
+    }
+
+    @Test
+    public void inc() throws Exception {
+
+    }
+
+    @Test
+    public void await() throws Exception {
+
+    }
+
+}
