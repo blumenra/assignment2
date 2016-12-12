a25e14e0284bfd95cc14c8b2c4e0a14c67515e0d
Alon Blumenreich <alon@mail.com>
2016-12-12 21:12:30 +0200

    First commit

---
+++ src/test/java/bgu/spl/a2/DeferredTest.java
@@ -0,0 +1,31 @@
+package bgu.spl.a2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by blumenra on 12/12/16.
+ */
+public class DeferredTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void isResolved() throws Exception {
+
+    }
+
+    @Test
+    public void resolve() throws Exception {
+
+    }
+
+    @Test
+    public void whenResolved() throws Exception {
+
+    }
+
+}
