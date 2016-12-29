d64e60c0cc761e2143bb88f21287473a4c651b3e
Alon Blumenreich <alon@mail.com>
2016-12-30 00:11:06 +0200

    Created new class JsonParser for parsing the json file received as input

---
+++ src/main/java/bgu/spl/a2/sim/JsonParser.java
@@ -0,0 +1,26 @@
+package bgu.spl.a2.sim;
+
+import bgu.spl.a2.sim.conf.ManufactoringPlan;
+import bgu.spl.a2.sim.tools.Tool;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by blumenra on 12/29/16.
+ */
+public class JsonParser {
+
+    private final int threads;
+    private final List<Tool> tools;
+    private final Map<Tool, int> tools;
+    private final List<ManufactoringPlan> plans;
+    private final List<String[]> waves;
+
+
+    public JsonParser(String jsonFileName) {
+
+
+    }
+
+}
