dc63c07f27c48b2d3de5662c6c232033bac860cd
Alon Blumenreich <alon@mail.com>
2016-12-31 00:07:21 +0200

    Checked finished tasks from masterPlan

--- removeMe/masterPlan2.java
+++ removeMe/masterPlan2.java
@@ -2,13 +2,13 @@
 		--implement JSON
 		--implement the Simulator
 		--create tool classes
-		-create output file
+		--create output file
 		check everything in Terminal with Maven
 		handle all the TODOs in the code
 		remove all souts (has a keyboard shortcut in the tips)
 		remove unused imports, variables any anything else unused in the code (has a keyboard shortcut in the tips)
-		Fixe the part id's, they dont match the output file and we dont know why...
-		implement the pool method od simulator
+		--Fixe the part id's, they dont match the output file and we dont know why...
+		--implement the pool method od simulator
 		--create classes for all our tasks
 
 */
